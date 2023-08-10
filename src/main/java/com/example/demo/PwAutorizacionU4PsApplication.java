b2712b2db50c7fc75d7fe5e8b886e6fa38e662f7
Pablo Suntaxi <pbsun.ale@gmail.com>
2023-08-09 19:47:23 -0500

    Taller 38

---
+++ src/main/java/com/example/demo/PwAutorizacionU4PsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4PsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4PsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/PwAutorizacionU4PsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionU4PsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
